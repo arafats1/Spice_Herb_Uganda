86e033d11601f12fea22b4fd1a8a4e4cf1f17d6d
Arafat <arafats144@gmail.com>
2023-01-26 21:53:24 +0300

    add react queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
+import queryClient from "../lib/query";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
 }
+
+export default MyApp;
