27458f78aa59f5f36ab2dceff685e48d9490b096
Arafat <arafats144@gmail.com>
2023-01-27 21:37:46 +0300

    change api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 const stripeServerSide = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2022-08-01",
+    apiVersion: "2022-11-15",
 });
 
 export { stripeServerSide };
