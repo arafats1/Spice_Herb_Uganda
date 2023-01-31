868428c580cb26eda58a4153ae2ab07f577968f0
Arafat <arafats144@gmail.com>
2023-01-31 16:52:05 +0300

    spices seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -189,7 +189,7 @@ const main = async () => {
     //         description: "Nutmeg is a seed and spice from the tropical tree Myristica fragrans, native to the Banda Islands in Indonesia. The tree is also known by the names nutmeg mace, mace, and nutmeg tree.",
     //         price: "8,000 per Kg",
     //         quantity: 10,
-    //         image: "https://res.cloudinary.com/druug5tgz/image/upload/v1675167949/Spices/nutmeg_mttspf.png",
+    //         image: "https://res.cloudinary.com/druug5tgz/image/upload/v1675171282/Spices/nutmeg_2_ar3rkp.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -229,7 +229,7 @@ const main = async () => {
     //         description: "Star anise is the star-shaped fruit of the evergreen tree Illicium verum, native to China. It is used whole or ground as a spice in Chinese, Japanese, Korean, Vietnamese, and Thai cuisines.",
     //         price: "7,000 per Kg",
     //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167948/Spices/star_anise_ozqjxw.png",
+    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167949/Spices/star_anise_wqkkai.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -246,7 +246,7 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Red Chilli",
-    //         description: "Red chilli is a spice made from the dried ripe fruit of plants belonging to the genus Capsicum. It is used to add a spicy flavour to dishes. The fruit is used fresh, dried, powdered, or ground into a paste.",
+    //         description: "Red chilli is a spice made from the dried ripe fruit of plants belonging to the genus Capsicum. It is used to add a spicy flavour to dishes.",
     //         price: "5,000 per Kg",
     //         quantity: 10,
     //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167948/Spices/red_chili_mtjeoa.png",
@@ -266,10 +266,10 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Ginger",
-    //         description: "Ginger is a flowering plant whose rhizome, ginger root or ginger, is widely used as a spice or a folk medicine. It is a herbaceous perennial which grows annual pseudostems (false stems made of the rolled bases of leaves) about one meter tall bearing narrow leaf blades.",
+    //         description: "Ginger is a flowering plant whose rhizome, ginger root or ginger, is widely used as a spice or a folk medicine. It is a herbaceous perennial which grows annual pseudostems",
     //         price: "6,000 per Kg",
     //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167947/Spices/ginger_gdxs2j.png",
+    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675171290/Spices/ginger_2_mvrpjd.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -286,10 +286,10 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Cloves",
-    //         description: "Cloves are the aromatic flower buds of a tree in the family Myrtaceae, Syzygium aromaticum. The tree is native to the Moluccas, in Indonesia. The English word clove derives from Dutch klouwen, via Latin clavus, from Ancient Greek κλάβος (klábos), meaning nail.",
+    //         description: "Cloves are the aromatic flower buds of a tree in the family Myrtaceae, Syzygium aromaticum. The tree is native to the Moluccas, in Indonesia.",
     //         price: "8,000 per Kg",
     //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167946/Spices/cloves_c3prf4.png",
+    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675171586/Spices/cloves3_wotim1.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -306,7 +306,7 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "nigella seed",
-    //         description: "Nigella sativa, commonly known as black cumin, nigella, nutmeg flower, fennel flower, Roman coriander, or kalonji, is an annual flowering plant in the family Ranunculaceae, native to southwest and Central Asia.",
+    //         description: "Nigella sativa, commonly known as black cumin, nigella, nutmeg flower, fennel flower, Roman coriander, or kalonji, is native to southwest and Central Asia.",
     //         price: "7,000 per Kg",
     //         quantity: 10,
     //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167946/Spices/nigella_seeds_wvhvrq.webp",
@@ -326,7 +326,7 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "gooseberry",
-    //         description: "Gooseberry is a common name for several species of deciduous shrubs in the genus Ribes in the family Grossulariaceae. The name is also used for the fruit of these plants. The fruit is usually green, yellow, or red, and is often used in jams and jellies.",
+    //         description: "Gooseberry is a common name for several species of deciduous shrubs in the genus Ribes in the family Grossulariaceae. The name is also used for the fruit of these plants. ",
     //         price: "5,000 per Kg",
     //         quantity: 10,
     //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167946/Spices/gooseberry_el4ozu.png",
@@ -346,7 +346,7 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Cardamom",
-    //         description: "Cardamom is a spice made from the seeds of several plants in the genera Elettaria and Amomum in the family Zingiberaceae. The two genera contain about 30 species, most of which are native to India, Sri Lanka, and other countries in South Asia.",
+    //         description: "Cardamom is a spice made from the seeds of several plants in the genera Elettaria and Amomum in the family Zingiberaceae. The two genera contain about 30 species.",
     //         price: "16,000 per Kg",
     //         quantity: 10,
     //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167946/Spices/cardamom_yzzobp.png",
@@ -366,10 +366,10 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Black Pepper",
-    //         description: "Black pepper is a flowering vine in the family Piperaceae, cultivated for its fruit, which is usually dried and used as a spice and seasoning. When dried, the fruit is known as a peppercorn. Black pepper is native to south India and is extensively cultivated there and elsewhere in tropical regions.",
+    //         description: "Black pepper is a flowering vine in the family Piperaceae, cultivated for its fruit, which is usually dried and used as a spice and seasoning.",
     //         price: "10,000 per Kg",
     //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167945/Spices/black_pepper_fomrpx.jpg",
+    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675172336/Spices/blackpepper2_pskghi.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -386,10 +386,10 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Coriander",
-    //         description: "Coriander, also known as cilantro or Chinese parsley, is an annual herb in the family Apiaceae. It is also known as dhania or kothmir in South Asia. Coriander is native to regions spanning from southern Europe and North Africa to southwestern Asia.",
+    //         description: "Coriander, also known as cilantro or Chinese parsley, is an annual herb in the family Apiaceae. It is also known as dhania or kothmir in Hindi, and as dhania or coriander in English.",
     //         price: "5,000 per Kg",
     //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167945/Spices/coriander_t4ajtj.jpg",
+    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675172786/Spices/cori_gpoy5z.png",
     //         category: {
     //             connectOrCreate: {
     //                 where: {
@@ -406,7 +406,7 @@ const main = async () => {
     // await primsa.product.create({
     //     data: {
     //         title: "Bay leaves",
-    //         description: "Bay leaves are the aromatic leaves of several kinds of laurel. The name is also used for the related trees, which are in the flowering plant genus Laurus. The leaves are used as a spice in cooking, and for fragrance in potpourri.",
+    //         description: "Bay leaves are the aromatic leaves of several kinds of laurel. The name is also used for the related trees, which are in the flowering plant genus Laurus.",
     //         price: "5,000 per Kg",
     //         quantity: 10,
     //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167945/Spices/bay_mdhm5z.png",
@@ -423,25 +423,25 @@ const main = async () => {
     //     },
     // });
 
-    // await primsa.product.create({
-    //     data: {
-    //         title: "Cumin",
-    //         description: "Cumin is a flowering plant in the family Apiaceae, native from the east Mediterranean to India. It is a herbaceous annual plant, about 50 cm (20 in) tall, with yellow flowers. The seeds are about 2 mm (0.079 in) long, grayish white, with striations.",
-    //         price: "5,000 per Kg",
-    //         quantity: 10,
-    //         image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675167945/Spices/cumin_ossk6k.jpg",
-    //         category: {
-    //             connectOrCreate: {
-    //                 where: {
-    //                     name: "Spices",
-    //                 },
-    //                 create: {
-    //                     name: "Spices",
-    //                 },
-    //             },
-    //         },
-    //     },
-    // });
+    await primsa.product.create({
+        data: {
+            title: "Cumin",
+            description: "Cumin is a flowering plant in the family Apiaceae, native from the east Mediterranean to India. It is a herbaceous annual plant, about 50 cm (20 in) tall, with yellow flowers.",
+            price: "5,000 per Kg",
+            quantity: 10,
+            image:"https://res.cloudinary.com/druug5tgz/image/upload/v1675173063/Spices/cumin2_tguo5g.png",
+            category: {
+                connectOrCreate: {
+                    where: {
+                        name: "Spices",
+                    },
+                    create: {
+                        name: "Spices",
+                    },
+                },
+            },
+        },
+    });
 
 
 
